492ba1b80c277329224944b4ab140d8aae380ad3
Roista57 <91594792+Roista57@users.noreply.github.com>
2024-04-04 16:34:25 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 268 ms, Memory: 39960 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,20 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M, D;
-	static boolean[] combSelect;
+	static int N;
+	static int M;
+	static int D;
+
+	static int[] di = { 0, -1, 0 };
+	static int[] dj = { -1, 0, +1 };
+
+	static int[] select;
+	static int max;
+
 	static int[][] map;
-	static int[][] temp;
-	static boolean[][] select;
-	static int MAX;
-	static Point[] archers;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,13 +26,9 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		D = Integer.parseInt(st.nextToken());
 
-		combSelect = new boolean[M];
-
-		map = new int[N + 1][M];
-		select = new boolean[N + 1][M];
-		MAX = Integer.MIN_VALUE;
-
-		archers = new Point[3];
+		map = new int[N + 1][M]; // 맵 가장 아래에 궁수를 둘거라 1 키워서 만들었음.
+		select = new int[3];
+		max = Integer.MIN_VALUE;
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -37,136 +36,114 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
 		comb(0, 0);
-		System.out.println(MAX);
+		System.out.println(max);
 	}
 
-	static void func(int n, int cnt) {
-		if (n == 0) {
-			MAX = Math.max(MAX, cnt);
-			return;
-		}
-		for (int p = 0; p < 3; p++) { // 0번 궁수부터 2번 궁수까지
-			List<Point> list = new ArrayList<>(); // 적의 위치 정보를 담는 리스트
-			for (int d = 1; d <= D; d++) { // 사거리가 1부터 D까지의 탐색
-				for (int i = n; i >= 0; i--) {
-					for (int j = 0; j < M; j++) {
-						int len = Math.abs(archers[p].i - i) + Math.abs(archers[p].j - j); // 적과 궁수와의 거리
-						// 거리가 d와 같음, 적이 있음, 아처와 같은 행에 있지 않은 경우
-						if (len == d && temp[i][j] == 1 && archers[p].i != i) {
-							list.add(new Point(i, j, len));
+	static Point find(Point start, int[][] map) {
+		boolean[][] visited = new boolean[N + 1][M];
+
+		Queue<Point> queue = new ArrayDeque<>();
+		queue.offer(start);
+		visited[start.i][start.j] = true;
+
+		int len = 1;
+
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+
+			while (--size >= 0) {
+				Point p = queue.poll();
+				for (int d = 0; d < 3; d++) {
+					int ni = p.i + di[d];
+					int nj = p.j + dj[d];
+
+					if (ni >= 0 && ni <= N && nj >= 0 && nj < M && !visited[ni][nj]) {
+						if (map[ni][nj] == 1) {
+							return new Point(ni, nj);
 						}
+						visited[ni][nj] = true;
+						queue.offer(new Point(ni, nj));
 					}
 				}
-				// 저장된 적중 거리가 짧고 가장 왼쪽에 있는 적을 찾는다.
-				if (!list.isEmpty()) {
-					Collections.sort(list);
-					int i = list.get(0).i;
-					int j = list.get(0).j;
-					if (!select[i][j])
-						cnt++;
-					select[i][j] = true;
-					break;
-				}
 			}
+			len++;
+			if (len > D)
+				break;
 		}
-
-		// 궁수에게 공격받은 적을 0으로 만들어줌
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (select[i][j]) {
-					temp[i][j] = 0;
-					select[i][j] = false;
-				}
-			}
-		}
-
-		// 궁수의 행을 앞으로 한칸 당겨줌
-		for (int p = 0; p < 3; p++) {
-			temp[archers[p].i--][archers[p].j] = 0;
-			temp[archers[p].i][archers[p].j] = 2;
-		}
-
-//		for (int[] a : temp) {
-//			System.out.println(Arrays.toString(a));
-//		}
-//		System.out.println("=====================");
-//		System.out.println();
-
-		func(n - 1, cnt);
+		return null;
 	}
 
-	static void comb(int idx, int cnt) { // 궁수의 위치를 M개중 3개를 뽑는 조합
-		if (cnt == 3) {
-			int index = 0;
-			temp = copyMap(map);
-			for (int i = 0; i < M; i++) {
-				if (combSelect[i]) {
-					temp[N][i] = 2;
-					archers[index++] = new Point(N, i);
+	static int func() {
+		int total = 0;
+		int[][] map = mapCopy();
+		for (int k = 0; k < N; k++) {
+			ArrayList<Point> list = new ArrayList<>();
+			// 사정거리에 있는 적 찾음
+			for (int i = 0; i < 3; i++) {
+				Point point = find(new Point(N, select[i]), map);
+				if (point != null)
+					list.add(point);
+			}
+
+			// 적 제거
+			for (int i = 0; i < list.size(); i++) {
+				Point p = list.get(i);
+				if (map[p.i][p.j] == 1) {
+					map[p.i][p.j] = 0;
+					total++;
 				}
 			}
-//			for (int[] a : temp) {
-//				System.out.println(Arrays.toString(a));
-//			}
-//			System.out.println("=====================");
-//			System.out.println();
-			func(N, 0);
+			down(map);
+		}
+		return total;
+	}
+
+	static void down(int[][] map) {
+		for (int j = 0; j < M; j++) {
+			for (int i = N - 1; i >= 0; i--) {
+				if (i == 0)
+					map[i][j] = 0;
+				else
+					map[i][j] = map[i - 1][j];
+			}
+		}
+	}
+
+	static void comb(int idx, int cnt) {
+		if (cnt == 3) {
+			// 궁수를 뽑아주고 맵에 세팅
+			int num = func();
+			max = Math.max(max, num);
 			return;
 		}
+
 		if (idx == M)
 			return;
 
-		combSelect[idx] = true;
+		select[cnt] = idx;
 		comb(idx + 1, cnt + 1);
-		combSelect[idx] = false;
+		select[cnt] = 0;
 		comb(idx + 1, cnt);
-
 	}
 
-	static int[][] copyMap(int[][] arrays) { // 맵 배열 복사
-		int[][] temp = new int[arrays.length][arrays[0].length];
-		for (int i = 0; i < arrays.length; i++) {
-			for (int j = 0; j < arrays[0].length; j++) {
-				temp[i][j] = arrays[i][j];
-			}
-		}
-		return temp;
-	}
-
-	static class Point implements Comparable<Point> {
+	static class Point {
 		int i;
 		int j;
-		int len;
 
 		public Point(int i, int j) {
 			this.i = i;
 			this.j = j;
 		}
+	}
 
-		public Point(int i, int j, int len) {
-			this.i = i;
-			this.j = j;
-			this.len = len;
-		}
-
-		// 거리를 비교하고 거리가 같다면 j의 값을 비교해 오름차순으로 정렬한다.
-		@Override
-		public int compareTo(Point o) {
-			if (len > o.len) {
-				return 1;
-			} else if (len == o.len) {
-				if (j > o.j) {
-					return 1;
-				} else if (j == o.j) {
-					return 0;
-				} else {
-					return -1;
-				}
-			} else {
-				return -1;
+	static int[][] mapCopy() {
+		int[][] temp = new int[N + 1][M];
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j < M; j++) {
+				temp[i][j] = map[i][j];
 			}
 		}
+		return temp;
 	}
 }
