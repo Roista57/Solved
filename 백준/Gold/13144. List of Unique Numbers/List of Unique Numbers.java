fb3689b2741475d7d7a6c4da13e13547a1cc0504
Roista57 <91594792+Roista57@users.noreply.github.com>
2024-07-05 15:32:39 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 300 ms, Memory: 25344 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -1,42 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] list = new int[N];
-		int MAX = 0;
+    public static void main(String[] args) throws IOException {
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			list[i] = Integer.parseInt(st.nextToken());
-			MAX = Math.max(MAX, list[i]);
-		}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-		int left = 0;
-		int right = 0;
-		long cnt = 0;
-		int[] select = new int[MAX + 1];
+        int[] arr = new int[N];
+        int max = 0;
 
-		long total = 0;
-		while (left < N && right < N) {
-			if (select[list[right]] != 0 && left != right) {
-				long addNum = (cnt * (cnt + 1)) / 2;
-				long subNum = ((cnt - 1) * ((cnt - 1) + 1)) / 2;
-				total += addNum - subNum;
-				cnt--;
-				select[list[left++]]--;
-			} else {
-				select[list[right++]]++;
-				cnt++;
-			}
-		}
-		long addNum = (cnt * (cnt + 1)) / 2;
-		total += addNum;
-		System.out.println(total);
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(arr[i], max);
+        }
+
+        int[] used = new int[max + 1];
+
+        int right = 0;
+        int left = 0;
+        long cnt = 0;
+        while (right < N) {
+            if (used[arr[right]] == 1) {
+                used[arr[left]] = 0;
+                left++;
+            } else {
+                used[arr[right]] = 1;
+                cnt += right-left+1;
+                right++;
+            }
+        }
+        System.out.println(cnt);
+
+    }
 }
