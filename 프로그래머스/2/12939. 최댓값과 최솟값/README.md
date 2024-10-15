e48c4c1ac95d7b01befc66fc39ab64e650209864
Roista57 <91594792+Roista57@users.noreply.github.com>
2024-10-15 09:23:38 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.15 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,10 +1,10 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
 
 ### 성능 요약
 
-메모리: 86.5 MB, 시간: 11.87 ms
+메모리: 89.8 MB, 시간: 12.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 14일 20:05:15
+2024년 10월 15일 09:23:35
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -4,15 +4,43 @@ class Solution {
     public String solution(String s) {
         
         String[] ans = s.split(" ");
-        int max = Integer.parseInt(ans[0]);
-        int min = Integer.parseInt(ans[0]);
         
+        Queue<MinNumber> minQueue = new PriorityQueue<>();
         for(int i=0;i<ans.length;i++){
-            max = Math.max(max, Integer.parseInt(ans[i]));
-            min = Math.min(min, Integer.parseInt(ans[i]));
+            minQueue.offer(new MinNumber(Integer.parseInt(ans[i])));
         }
         
-        String answer = min +" "+ max;
+        Queue<MaxNumber> maxQueue = new PriorityQueue<>();
+        for(int i=0;i<ans.length;i++){
+            maxQueue.offer(new MaxNumber(Integer.parseInt(ans[i])));
+        }
+        
+        String answer = maxQueue.peek().num +" "+ minQueue.peek().num;
         return answer;
+    }
+}
+
+class MinNumber implements Comparable<MinNumber>{
+    int num;
+    
+    public MinNumber(int num){
+        this.num = num;
+    }
+    
+    @Override
+    public int compareTo(MinNumber o){
+        return o.num - num;
+    }
+}
+
+class MaxNumber implements Comparable<MaxNumber>{
+    int num;
+    public MaxNumber(int num){
+        this.num = num;
+    }
+    
+    @Override
+    public int compareTo(MaxNumber o){
+        return num - o.num;
     }
 }
