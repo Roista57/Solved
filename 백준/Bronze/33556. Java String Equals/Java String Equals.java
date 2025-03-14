59dff6104855f7cd34f2cb822cb83c925eae2a65
Roista57 <91594792+Roista57@users.noreply.github.com>
2025-03-15 00:56:59 +0900

    [Bronze I] Title: Java String Equals, Time: 88 ms, Memory: 12760 KB -BaekjoonHub

---
+++ 백준/Bronze/33556. Java String Equals/Java String Equals.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String A = sc.nextLine();
+		String B = sc.nextLine();
+		if (A.equals("null")) {
+			System.out.println("NullPointerException");
+			System.out.println("NullPointerException");
+		} else {
+			if (B.equals("null")) {
+				System.out.println(false);
+				System.out.println(false);
+			} else {
+				System.out.println(A.equals(B));
+				System.out.println(A.equalsIgnoreCase(B));
+			}
+		}
+	}
+}
