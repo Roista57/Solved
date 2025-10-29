127f86ec6d2a2dec7eb2565cd904fcb6d25370fb
Roista57 <91594792+Roista57@users.noreply.github.com>
2025-10-29 17:04:06 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 72 ms, Memory: 11624 KB -BaekjoonHub

--- 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -4,12 +4,14 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		for(int i=1;i<=N*2-1;i++){
 			for(int j=0;j<N - Math.abs(i-N);j++){
-				System.out.print("*");
+				sb.append("*");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
+		System.out.println(sb.toString());
 	}
 }
