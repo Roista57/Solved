390d9013ac1f2687c5bb6ff277c6b5b1ca9c04ae
Roista57 <91594792+Roista57@users.noreply.github.com>
2025-11-12 16:19:48 +0900

    [Bronze IV] Title: 레퓨닛의 덧셈, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/33964. 레퓨닛의 덧셈/레퓨닛의 덧셈.java
+++ 백준/Bronze/33964. 레퓨닛의 덧셈/레퓨닛의 덧셈.java
@@ -7,23 +7,21 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int X = Integer.parseInt(st.nextToken());
 		int Y = Integer.parseInt(st.nextToken());
-		StringBuilder sb = new StringBuilder();
 
 		if(X > Y){
 			for(int i=0;i<X-Y;i++){
-				sb.append("1");
+				System.out.print(1);
 			}
 			for(int i=0;i<Y;i++){
-				sb.append("2");
+				System.out.print(2);
 			}
 		}else{
 			for(int i=0;i<Y-X;i++){
-				sb.append("1");
+				System.out.print(1);
 			}
 			for(int i=0;i<X;i++){
-				sb.append("2");
+				System.out.print(2);
 			}
 		}
-		System.out.println(sb.toString());
 	}
 }
