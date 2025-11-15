3bdfc51bf37e761a5539874e7b64a56091b86bff
Roista57 <91594792+Roista57@users.noreply.github.com>
2025-11-15 13:48:30 +0900

    [Bronze III] Title: 쿠폰, Time: 64 ms, Memory: 11848 KB -BaekjoonHub

---
+++ 백준/Bronze/10179. 쿠폰/쿠폰.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		for(int i=0;i<N;i++){
+			double cost = Double.parseDouble(br.readLine()) * 0.8;
+			System.out.printf("$%.2f\n", cost);
+		}
+	}
+}
