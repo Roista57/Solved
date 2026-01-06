a5a45db93e8b48417fb5b7cab1d3a7a043a78b75
Roista57 <91594792+Roista57@users.noreply.github.com>
2026-01-06 21:24:25 +0900

    [Bronze IV] Title: !밀비 급일, Time: 60 ms, Memory: 11452 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while(true){
+			String line = br.readLine();
+			if(line.equals("END")){
+				break;
+			}
+			sb.append(new StringBuilder(line).reverse()).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
